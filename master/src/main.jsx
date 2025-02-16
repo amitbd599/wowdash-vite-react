96258fa531207fdf6cad78e5824e8f94a56fc3e3
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-02-16 11:19:37 +0600

    Initial commit

---
+++ master/src/main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "react-quill/dist/quill.snow.css";
+import "jsvectormap/dist/css/jsvectormap.css";
+import "react-toastify/dist/ReactToastify.css";
+import "react-modal-video/css/modal-video.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);
