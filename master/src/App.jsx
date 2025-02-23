72632dda253f301fb23d092d01ae056f2d1f92bc
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-02-23 12:27:08 +0600

    portfolio add

--- master/src/App.jsx
+++ master/src/App.jsx
@@ -49,7 +49,7 @@ import NotificationPage from "./pages/NotificationPage";
 import PaginationPage from "./pages/PaginationPage";
 import PaymentGatewayPage from "./pages/PaymentGatewayPage";
 import PieChartPage from "./pages/PieChartPage";
-// import PortfolioPage from "./pages/PortfolioPage";
+import PortfolioPage from "./pages/PortfolioPage";
 // import PricingPage from "./pages/PricingPage";
 // import ProgressPage from "./pages/ProgressPage";
 // import RadioPage from "./pages/RadioPage";
@@ -187,8 +187,8 @@ function App() {
         <Route exact path='/pagination' element={<PaginationPage />} />
         <Route exact path='/payment-gateway' element={<PaymentGatewayPage />} />
         <Route exact path='/pie-chart' element={<PieChartPage />} />
-        {/*      <Route exact path='/portfolio' element={<PortfolioPage />} />
-        <Route exact path='/pricing' element={<PricingPage />} />
+        <Route exact path='/portfolio' element={<PortfolioPage />} />
+        {/*    <Route exact path='/pricing' element={<PricingPage />} />
         <Route exact path='/progress' element={<ProgressPage />} />
         <Route exact path='/radio' element={<RadioPage />} />
         <Route exact path='/role-access' element={<RoleAccessPage />} />
